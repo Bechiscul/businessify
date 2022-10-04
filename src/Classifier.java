a39d22786946001d9bc82c1f6b75939c5b8d4938
Bech1337 <sebberas2@gmail.com>
2022-10-04 16:36:51 +0200

    initial commit

---
+++ src/Classifier.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+// classify adjectives and nouns
+
+public class Classifier {
+    static HashSet<String> nouns = new HashSet<>();
+    static HashSet<String> adjectives = new HashSet<>();
+    static HashSet<String> verbs = new HashSet<>();
+
+
+    public static void classify(String text)  {
+        
+    }
+
+    private static ArrayList<String> getSentences(String text) {
+        ArrayList<String> sentences = new ArrayList<>();
+
+        int lastPeriod = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '.') {
+                // Because String.substring is inclusive, we must increment it to get rid of the '.'
+                // in the case where this sentence is not the first.
+                sentences.add(text.substring((lastPeriod != 0) ? lastPeriod + 1 : 0, i).trim());
+                lastPeriod = i;
+            }
+        }
+
+        // If the last sentence is not delimited by a '.', then treat it as a sentence.
+        if (text.lastIndexOf('.') != text.length() - 1) {
+            sentences.add(text.substring(lastPeriod + 1).trim());
+        }
+
+        return sentences;
+    }
+
+}
