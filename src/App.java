5b551c9d553604516880e9ba9ac6e7fc3a785595
Bech1337 <sebberas2@gmail.com>
2022-10-11 12:55:12 +0200

    classify interface

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Classifier classifier = new Classifier();
--- src/Classifier.java
+++ src/Classifier.java
@@ -1,43 +1,41 @@
+import edu.mit.jwi.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.ArrayDeque;
 
-import edu.mit.jwi.*;
 
 // classify adjectives, nouns and verbs
 public class Classifier {
-    private IDictionary dictionary;
+  private IDictionary dictionary;
 
+  public Classifier() throws IOException {
+    File file = new File("./assets/dict");
+    this.dictionary = new Dictionary(file);
+    this.dictionary.open();
+  }
 
-    public Classifier() throws IOException {
-        File file = new File("./assets/dict");
-        this.dictionary = new Dictionary(file);
-        this.dictionary.open();
+  public ArrayDeque<String> classify(String text) {
+    return new ArrayDeque<>();
+  }
+
+  private static ArrayList<String> getSentences(String text) {
+    ArrayList<String> sentences = new ArrayList<>();
+
+    int lastPeriod = 0;
+    for (int i = 0; i < text.length(); i++) {
+      if (text.charAt(i) == '.') {
+        // Because String.substring is inclusive, we must increment it to get rid of the '.'
+        // in the case where this sentence is not the first.
+        sentences.add(text.substring((lastPeriod != 0) ? lastPeriod + 1 : 0, i).trim());
+        lastPeriod = i;
+      }
     }
 
-
-    public static void classify(String text)  {
-        
+    // If the last sentence is not delimited by a '.', then treat it as a sentence.
+    if (text.lastIndexOf('.') != text.length() - 1) {
+      sentences.add(text.substring(lastPeriod + 1).trim());
     }
 
-    private static ArrayList<String> getSentences(String text) {
-        ArrayList<String> sentences = new ArrayList<>();
-
-        int lastPeriod = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == '.') {
-                // Because String.substring is inclusive, we must increment it to get rid of the '.'
-                // in the case where this sentence is not the first.
-                sentences.add(text.substring((lastPeriod != 0) ? lastPeriod + 1 : 0, i).trim());
-                lastPeriod = i;
-            }
-        }
-
-        // If the last sentence is not delimited by a '.', then treat it as a sentence.
-        if (text.lastIndexOf('.') != text.length() - 1) {
-            sentences.add(text.substring(lastPeriod + 1).trim());
-        }
-
-        return sentences;
-    }
-
+    return sentences;
+  }
 }
